437ab83188e6c4ef1e32d754f23518cc9391e658
Grzegorz Kasprzyszak <gregorkmail@gmail.com>
2017-03-05 18:09:17 +0100

    Incorrect calculated diff time.

--- src/svc_kernel/svc_time.c
+++ src/svc_kernel/svc_time.c
@@ -9,6 +9,14 @@ struct timespec timerStart(void) {
 unsigned long long timerStop(struct timespec startTime) {
     struct timespec stopTime;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stopTime);
-    unsigned long long diffInNanos = (unsigned long long)stopTime.tv_nsec - (unsigned long long)startTime.tv_nsec;
+    struct timespec resultTime;
+    if((stopTime.tv_nsec - startTime.tv_nsec) < 0) {
+    	resultTime.tv_sec = stopTime.tv_sec - startTime.tv_sec - 1;
+    	resultTime.tv_nsec = stopTime.tv_nsec - startTime.tv_nsec + 1000000000;
+    } else {
+    	resultTime.tv_sec = stopTime.tv_sec - startTime.tv_sec;
+    	resultTime.tv_nsec = stopTime.tv_nsec - startTime.tv_nsec;
+    }
+    unsigned long long diffInNanos = resultTime.tv_sec*1000000000 + resultTime.tv_nsec;
     return diffInNanos;
 }
