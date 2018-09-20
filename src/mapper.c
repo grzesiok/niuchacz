d37a881f2d9690b858e3d367d6c23a007ae73f12
Grzegorz <gregorkmail@gmail.com>
2018-09-20 07:34:45 +0200

    Add payload creation with headers of higher level for exmple TCP/UDP/ICMP

--- src/mapper.c
+++ src/mapper.c
@@ -6,6 +6,7 @@
 bool mapFrame(unsigned char *frame, size_t framelen, PMAPPER_RESULTS presults) {
 	int size_ip;
 	const struct mapper_ip *ip = &presults->_ip;
+        char *payload;
 
 	/* map header to ethernet structure */
 	memcpy(&presults->_ethernet, frame, sizeof(struct mapper_ethernet));
@@ -16,5 +17,9 @@ bool mapFrame(unsigned char *frame, size_t framelen, PMAPPER_RESULTS presults) {
 		SYSLOG(LOG_ERR, "   * Invalid IP header length: %u bytes", size_ip);
 		return false;
 	}
+        payload = (char*)(frame + SIZE_ETHERNET + size_ip);
+        sprintf(presults->_payload_16b, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+                payload[0], payload[1], payload[2], payload[3], payload[4], payload[5], payload[6], payload[7],
+                payload[8], payload[9], payload[10], payload[11], payload[12], payload[13], payload[14], payload[15]);
 	return true;
 }
