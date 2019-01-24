109cc24e90f8e128fa200505de855fa086d59a2c
Grzegorz <gregorkmail@gmail.com>
2019-01-24 21:00:05 +0100

    Rwrite statistics management

--- src/svc_kernel/svc_statistics.c
+++ src/svc_kernel/svc_statistics.c
@@ -2,145 +2,92 @@
 #include "svc_kernel/svc_lock.h"
 #include "algorithms.h"
 
-#define STATS_ENTRY_FLAGS_FREE 0
-#define STATS_ENTRY_FLAGS_USED 1
+typedef struct {
+    PDOUBLYLINKEDLIST _statsList;
+} stats_mgr_t;
 
-typedef struct _STATS_ENTRY {
-	unsigned char _flags;
-	char _statsName[STATS_ENTRY_NAME_MAXSIZE];
-	int _type;
-	unsigned long long _value;
-} STATS_ENTRY, *PSTATS_ENTRY;
+stats_mgr_t g_statsMgr;
 
-#define STATS_BLOCK_MAX_ENTRIES 32
-
-typedef struct _STATS_LEAFBLOCK {
-	STATS_ENTRY _p_entries[STATS_BLOCK_MAX_ENTRIES];
-	struct _STATS_LEAFBLOCK *_p_nextBlock;
-} STATS_LEAFBLOCK, *PSTATS_LEAFBLOCK;
-
-typedef struct _STATS_ROOTBLOCK {
-	LOCK_VAR;
-	STATS_LEAFBLOCK _leafBlock;
-} STATS_ROOTBLOCK, *PSTATS_ROOTBLOCK;
-
-STATS_ROOTBLOCK g_StatsRootBlock;
 
 //internal API
-static PSTATS_ENTRY i_statsFindEntry(PSTATS_LEAFBLOCK p_leafBlock, unsigned char expectedFlags, unsigned char desiredFlags) {
-	int i;
-	for(i = 0;i < STATS_BLOCK_MAX_ENTRIES;i++) {
-		if(p_leafBlock->_p_entries[i]._flags != expectedFlags) {
-			continue;
-		}
-		if(!__atomic_compare_exchange(&p_leafBlock->_p_entries[i]._flags, &expectedFlags, &desiredFlags, false, __ATOMIC_ACQUIRE, __ATOMIC_ACQUIRE))
-		{
-			continue;
-		}
-		return &p_leafBlock->_p_entries[i];
-	}
-	return NULL;
-}
 
 //external API
-KSTATUS statsStart(void) {
-	LOCK_INIT(&g_StatsRootBlock, STATS_ROOTBLOCK, "STATS_ROOTBLOCK");
-	return KSTATUS_SUCCESS;
+KSTATUS statsMgrStart(void) {
+    SYSLOG(LOG_INFO, "[STATSMGR] Starting ...");
+    g_statsMgr._statsList = doublylinkedlistAlloc();
+    if(g_statsMgr._statsList == NULL)
+        return KSTATUS_UNSUCCESS;
+    return KSTATUS_SUCCESS;
 }
 
-void statsStop(void) {
-	//
+void statsMgrStop(void) {
+    SYSLOG(LOG_INFO, "[STATSMGR] Stopping ...");
+    SYSLOG(LOG_INFO, "[STATSMGR] Cleaning up...");
+    doublylinkedlistFreeDeletedEntries(g_statsMgr._statsList);
+    doublylinkedlistFree(g_statsMgr._statsList);
 }
 
-KSTATUS statsAlloc(const char* statsName, int type, stats_key *p_key) {
-	SYSLOG(LOG_INFO, "statsAlloc(%s[%zd], %d, %p)", statsName, strlen(statsName), type, p_key);
-	if(strlen(statsName) >= STATS_ENTRY_NAME_MAXSIZE)
-		return KSTATUS_UNSUCCESS;
-	stats_key statsKey = statsFind(statsName);
-	if(statsKey != NULL){
-		*p_key = statsKey;
-		return KSTATUS_SUCCESS;
-	}
-	KSTATUS _status = KSTATUS_UNSUCCESS;
-	LOCK(&g_StatsRootBlock, STATS_ROOTBLOCK);
-	PSTATS_LEAFBLOCK p_leafBlock = &g_StatsRootBlock._leafBlock;
-	while(p_leafBlock != NULL) {
-		PSTATS_ENTRY p_entry = i_statsFindEntry(p_leafBlock, STATS_ENTRY_FLAGS_FREE, STATS_ENTRY_FLAGS_USED);
-		if(p_entry != NULL) {
-			strncpy(p_entry->_statsName, statsName, STATS_ENTRY_NAME_MAXSIZE);
-			p_entry->_type = type;
-			p_entry->_value = 0;
-			statsKey = p_entry;
-			_status = KSTATUS_SUCCESS;
-			break;
-		}
-		if(p_leafBlock->_p_nextBlock == NULL) {
-			p_leafBlock->_p_nextBlock = MALLOC(STATS_LEAFBLOCK, 1);
-			memset(p_leafBlock->_p_nextBlock, 0, sizeof(STATS_LEAFBLOCK));
-		}
-		p_leafBlock = p_leafBlock->_p_nextBlock;
-	}
-	UNLOCK(&g_StatsRootBlock, STATS_ROOTBLOCK);
-	*p_key = statsKey;
-	return _status;
+stats_list_t* statsCreate(const char* listName) {
+    stats_list_t* p_tmp_stats;
+    stats_list_t* p_stats;
+    uint32_t additional_space;
+
+    additional_space = strlen(listName)+1;
+    p_tmp_stats = MALLOC2(stats_list_t, 1, additional_space);
+    if(p_tmp_stats == NULL)
+        return NULL;
+    memset(p_tmp_stats, 0, sizeof(stats_list_t)+additional_space);
+    p_tmp_stats->_name = memoryPtrMove(p_tmp_stats, sizeof(stats_list_t));
+    strcpy(p_tmp_stats->_name, listName);
+    if(art_tree_init(&p_tmp_stats->_tree) != 0) {
+        FREE(p_tmp_stats);
+        return NULL;
+    }
+    SYSLOG(LOG_INFO, "statsCreate(%s) adding To List ...", p_tmp_stats->_name);
+    p_stats = doublylinkedlistAdd(g_statsMgr._statsList, *((uint64_t*)listName), p_tmp_stats, sizeof(stats_list_t)+additional_space);
+    p_stats->_name = memoryPtrMove(p_stats, sizeof(stats_list_t));
+    FREE(p_tmp_stats);
+    SYSLOG(LOG_INFO, "statsCreate(%s) added ...", p_stats->_name);
+    return p_stats;
 }
 
-void statsFree(stats_key key) {
-	if(key == NULL)
-		return;
-	PSTATS_ENTRY p_entry = (PSTATS_ENTRY)key;
-	SYSLOG(LOG_INFO, "statsFree(%s)", p_entry->_statsName);
-	p_entry->_flags = STATS_ENTRY_FLAGS_FREE;
+void statsDestroy(stats_list_t* p_stats) {
+    SYSLOG(LOG_INFO, "statsDestroy(%s)", p_stats->_name);
+    doublylinkedlistDel(g_statsMgr._statsList, p_stats);
+    art_tree_destroy(&p_stats->_tree);
+    doublylinkedlistRelease(p_stats);
 }
 
-unsigned long long statsUpdate(stats_key key, unsigned long long value) {
-	if(key == NULL)
-		return 0;
-	PSTATS_ENTRY p_entry = (PSTATS_ENTRY)key;
-	switch(p_entry->_type) {
-	case STATS_TYPE_SUM:
-		return __atomic_add_fetch(&p_entry->_value, value, __ATOMIC_RELEASE);
-	case STATS_TYPE_LAST:
-		__atomic_store(&p_entry->_value, &value, __ATOMIC_RELEASE);
-		return value;
-	}
-	return 0;
+KSTATUS statsAlloc(stats_list_t* p_stats, const char* statsName, int flags, stats_entry_t* p_entry) {
+    SYSLOG(LOG_INFO, "statsAlloc(%s, %s[%zd], flags=%d, ptr=%p)", p_stats->_name, statsName, strlen(statsName), flags, p_entry);
+    p_entry->_flags = flags;
+    p_entry->_value = 0;
+    if(art_insert(&p_stats->_tree, (const unsigned char*)statsName, strlen(statsName), p_entry) == p_entry) {
+        return KSTATUS_UNSUCCESS;
+    }
+    return KSTATUS_SUCCESS;
 }
 
-stats_key statsFind(const char* statsName) {
-	stats_key statsKey = NULL;
-	LOCK(&g_StatsRootBlock, STATS_ROOTBLOCK);
-	PSTATS_LEAFBLOCK p_leafBlock = &g_StatsRootBlock._leafBlock;
-	while(p_leafBlock != NULL) {
-		PSTATS_ENTRY p_entry = i_statsFindEntry(p_leafBlock, STATS_ENTRY_FLAGS_USED, STATS_ENTRY_FLAGS_USED);
-		if(p_entry != NULL && strlen(p_entry->_statsName) == strlen(statsName) && strncmp(p_entry->_statsName, statsName, strlen(p_entry->_statsName)) == 0) {
-			statsKey = p_entry;
-			break;
-		}
-		p_leafBlock = p_leafBlock->_p_nextBlock;
-	}
-	UNLOCK(&g_StatsRootBlock, STATS_ROOTBLOCK);
-	return statsKey;
+void statsFree(stats_list_t* p_stats, const char* statsName) {
+    SYSLOG(LOG_INFO, "statsFree(%s, %s)", p_stats->_name, statsName);
+    art_delete(&p_stats->_tree, (const unsigned char*)statsName, strlen(statsName));
 }
 
-unsigned long long statsGetValue(stats_key key) {
-	if(key == NULL)
-		return 0;
-	PSTATS_ENTRY p_entry = (PSTATS_ENTRY)key;
-	return p_entry->_value;
+stats_entry_t* statsFind(stats_list_t* p_stats, const char* statsName) {
+    return art_search(&p_stats->_tree, (const unsigned char*)statsName, strlen(statsName));
 }
 
-void statsDump(void) {
-	SYSLOG(LOG_INFO, "STATS_DUMP: START");
-	LOCK(&g_StatsRootBlock, STATS_ROOTBLOCK);
-	PSTATS_LEAFBLOCK p_leafBlock = &g_StatsRootBlock._leafBlock;
-	while(p_leafBlock != NULL) {
-		PSTATS_ENTRY p_entry = i_statsFindEntry(p_leafBlock, STATS_ENTRY_FLAGS_USED, STATS_ENTRY_FLAGS_USED);
-                if(p_entry != NULL) {
-			SYSLOG(LOG_INFO, "STAT[%s]=%llu", p_entry->_statsName, p_entry->_value);
-		}
-		p_leafBlock = p_leafBlock->_p_nextBlock;
-	}
-	UNLOCK(&g_StatsRootBlock, STATS_ROOTBLOCK);
-	SYSLOG(LOG_INFO, "STATS_DUMP: STOP");
+unsigned long long statsUpdate(stats_entry_t *p_entry, unsigned long long value) {
+    switch(p_entry->_flags) {
+        case STATS_FLAGS_TYPE_SUM:
+            return __atomic_add_fetch(&p_entry->_value, value, __ATOMIC_RELEASE);
+        case STATS_FLAGS_TYPE_LAST:
+            __atomic_store(&p_entry->_value, &value, __ATOMIC_RELEASE);
+            return value;
+    }
+    return 0;
+}
+
+unsigned long long statsGetValue(stats_entry_t* p_entry) {
+    return p_entry->_value;
 }
