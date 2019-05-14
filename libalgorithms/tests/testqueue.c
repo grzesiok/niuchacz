9d1436858f04133b2dbfb5c845015090edc0d031
Grzegorz <gregorkmail@gmail.com>
2019-05-14 20:48:44 +0200

    Update test script for queue

--- libalgorithms/tests/testqueue.c
+++ libalgorithms/tests/testqueue.c
@@ -4,17 +4,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <pthread.h>
+
+typedef struct {
+    pthread_t _id;
+    perf_results_t _perfResults;
+} test_thread_t;
+
+typedef struct {
+    perf_results_t _perfResults;
+    volatile size_t _seq;
+    size_t _numThreads;
+    volatile size_t _array[50000000];
+} test_cfg_t;
 
 queue_t *gpQueue;
+test_cfg_t gReadCfg;
+test_cfg_t gWriteCfg;
+
 size_t numElements = 3000000;
-volatile size_t readArray[50000000];
-volatile size_t writeArray[50000000];
-size_t numReadThreads = 3;
-pthread_t threadReadArray[3];
-size_t numWriteThreads = 4;
-pthread_t threadWriteArray[4];
-volatile size_t actualReadI;
-volatile size_t actualWriteI;
+test_thread_t threadReadArray[3];
+test_thread_t threadWriteArray[4];
 
 int randomGet(int min, int max) {
     int tmp;
@@ -31,37 +41,44 @@ int randomGet(int min, int max) {
 void* routineWrite(void* arg) {
     char buffer[1000];
     size_t i = 0;
-    size_t ret, size, writeTime = 0;
-    struct timespec startTime;
+    size_t ret, size;
+    test_thread_t *threadCfg = (test_thread_t*)arg;
+    perf_stats_t perfStats;
+    perf_results_t perfResults;
 
-    timerWatchStart(&startTime);
+    perfWatchThreadInit();
+    perfWatchStart(&perfStats);
     queue_producer_new(gpQueue);
     while(1) {
-        i = __atomic_add_fetch(&actualWriteI, 1, __ATOMIC_RELEASE);
+        i = __atomic_add_fetch(&gWriteCfg._seq, 1, __ATOMIC_RELEASE)-1;
         if(i >= numElements)
             break;
         size = randomGet(sizeof(i), 1000);
         *((size_t*)buffer) = i;
-        writeArray[i] = size;
+        gWriteCfg._array[i] = size;
         ret = queue_write(gpQueue, buffer, size, NULL);
         if(ret != size){
             printf("WrongSize[WRITE, i=%zu, orig=%zu, curr=%zu]\n", i, size, ret);
         }
     }
     queue_producer_free(gpQueue);
-    writeTime += timerWatchStop(startTime);
-    return (void*)writeTime;
+    perfResults = perfWatchStop(&perfStats);
+    perfWatchAdd(&threadCfg->_perfResults, &perfResults);
+    return NULL;
 }
 
 void* routineRead(void* arg) {
     char buffer[1000];
-    size_t readedI, readedSizeI, readTime = 0;
-    struct timespec startTime;
+    size_t readedI, readedSizeI;
+    test_thread_t *threadCfg = (test_thread_t*)arg;
+    perf_stats_t perfStats;
+    perf_results_t perfResults;
 
-    timerWatchStart(&startTime);
+    perfWatchThreadInit();
+    perfWatchStart(&perfStats);
     queue_consumer_new(gpQueue);
     while(1) {
-        if(__atomic_add_fetch(&actualReadI, 1, __ATOMIC_RELEASE) >= numElements)
+        if(__atomic_add_fetch(&gReadCfg._seq, 1, __ATOMIC_RELEASE)-1 >= numElements)
             break;
         readedSizeI = queue_read(gpQueue, buffer, NULL);
         if(readedSizeI == -1) {
@@ -72,14 +89,15 @@ void* routineRead(void* arg) {
         if(readedI >= numElements) {
             printf("readedI=%zu\n", readedI);
         }
-        if(readedSizeI != writeArray[readedI]) {
-            printf("WrongSize[READ, i=%zu, orig=%zu, curr=%zu]\n", readedI, writeArray[readedI], readedSizeI);
+        if(readedSizeI != gWriteCfg._array[readedI]) {
+            printf("WrongSize[READ, i=%zu, orig=%zu, curr=%zu]\n", readedI, gReadCfg._array[readedI], readedSizeI);
         }
-        __atomic_add_fetch(&readArray[readedI], 1, __ATOMIC_RELEASE);
+        __atomic_add_fetch(&gReadCfg._array[readedI], 1, __ATOMIC_RELEASE);
     }
     queue_consumer_free(gpQueue);
-    readTime += timerWatchStop(startTime);
-    return (void*)readTime;
+    perfResults = perfWatchStop(&perfStats);
+    perfWatchAdd(&threadCfg->_perfResults, &perfResults);
+    return NULL;
 }
 
 int main() {
@@ -87,53 +105,76 @@ int main() {
     int seed = time(&tt);
     srand(seed);
 
-    actualReadI = actualWriteI = 0;
-
-	size_t i;
-	printf("Queue:CREATE\n");
-	gpQueue = queue_create(1000000);
-	for(i = 0;i < numWriteThreads;i++) {
-		if(pthread_create(&threadWriteArray[i], NULL, routineWrite, NULL)) {
-			perror("Error creating thread\n");
-			return 1;
-		}
-	}
-	for(i = 0;i < numReadThreads;i++) {
-		if(pthread_create(&threadReadArray[i], NULL, routineRead, NULL)) {
-			perror("Error creating thread\n");
-			return 1;
-		}
-	}
-	struct timespec ts;
-	int ret;
-	size_t cumulativeReadTime = 0, cumulativeWriteTime = 0;
-	size_t readTime = 0, writeTime = 0;
-	for(i = 0;i < numReadThreads;i++) {
-		do {
-			if(ret == ETIMEDOUT) {
-				//printf("actualReadI=%zu\n", actualReadI);
-				//printf("actualWriteI=%zu\n", actualWriteI);
-			}
-			clock_gettime(CLOCK_REALTIME, &ts);
-			ts.tv_sec += 1;
-		} while((ret = pthread_timedjoin_np(threadReadArray[i], (void**)&readTime, &ts)) == ETIMEDOUT);
-		cumulativeReadTime += readTime;
-	}
-	for(i = 0;i < numWriteThreads;i++) {
-		pthread_tryjoin_np(threadWriteArray[i], (void**)&writeTime);
-		cumulativeWriteTime += writeTime;
-	}
-	printf("Queue:DESTROY\n");
-	queue_destroy(gpQueue);
-	for(i = 1;i <= numElements;i++) {
-		if(readArray[i] != 1)
-			printf("readArray[%zu] read %zu times insted once\n", i, readArray[i]);
-	}
-	//timerTimespecDiff(&readTimeStart, &ReadTimeEnd, &ReadTime);
-	//timerTimespecDiff(&writeTimeStart, &writeTimeEnd, &WriteTime);
-	printf("WRITE[TIME in s]: %f\n", (float)cumulativeWriteTime/1000000000.0f/(float)numWriteThreads);
-	printf("WRITE[THROUPGHPUT in item/s]: %zu\n", (size_t)((float)numElements/((float)cumulativeWriteTime/1000000000.0f)));
-	printf("READ[TIME in s]: %f\n", (float)cumulativeReadTime/1000000000.0f/(float)numReadThreads);
-	printf("READ[THROUPGHPUT in item/s]: %zu\n", (size_t)((float)numElements/((float)cumulativeReadTime/1000000000.0f)));
-	return 0;
+    // Configure test
+    gReadCfg._numThreads = 3;
+    gReadCfg._seq = 0;
+    gWriteCfg._numThreads = 4;
+    gWriteCfg._seq = 0;
+    // End Configuration
+    if(!perfWatchInit()) {
+        perror("Error during init PAPI:\n");
+        return 1;
+    }
+    size_t i;
+    printf("Queue:CREATE\n");
+    gpQueue = queue_create(1000000);
+    for(i = 0;i < gWriteCfg._numThreads;i++) {
+        if(pthread_create(&threadWriteArray[i]._id, NULL, routineWrite, &threadWriteArray[i])) {
+            perror("Error creating thread\n");
+            return 1;
+        }
+    }
+    for(i = 0;i < gReadCfg._numThreads;i++) {
+        if(pthread_create(&threadReadArray[i]._id, NULL, routineRead, &threadReadArray[i])) {
+            perror("Error creating thread\n");
+            return 1;
+        }
+    }
+    printf("Queue: <==STATS_DUMP==>\n");
+    printf("Queue: EntriesCurrent=%zu\n", gpQueue->_stats_EntriesCurrent);
+    printf("Queue: EntriesMax=%zu\n", gpQueue->_stats_EntriesMax);
+    printf("Queue: MemUsageCurrent=%zu\n", gpQueue->_stats_MemUsageCurrent);
+    printf("Queue: MemUsageMax=%zu\n", gpQueue->_stats_MemUsageMax);
+    printf("Queue: MemSizeCurrent=%zu\n", gpQueue->_stats_MemSizeCurrent);
+    printf("Queue: MemSizeMin=%zu\n", gpQueue->_stats_MemSizeMin);
+    printf("Queue: MemSizeMax=%zu\n", gpQueue->_stats_MemSizeMax);
+    struct timespec ts;
+    int ret;
+    uint64_t cumulativeReadTime = 0, cumulativeWriteTime = 0;
+    for(i = 0;i < gReadCfg._numThreads;i++) {
+        do {
+            if(ret == ETIMEDOUT) {
+                //printf("actualReadI=%zu\n", actualReadI);
+                //printf("actualWriteI=%zu\n", actualWriteI);
+            }
+            clock_gettime(CLOCK_REALTIME, &ts);
+            ts.tv_sec += 1;
+        } while((ret = pthread_timedjoin_np(threadReadArray[i]._id, NULL, &ts)) == ETIMEDOUT);
+        cumulativeReadTime += threadReadArray[i]._perfResults._time;
+    }
+    for(i = 0;i < gWriteCfg._numThreads;i++) {
+        pthread_tryjoin_np(threadWriteArray[i]._id, NULL);
+        cumulativeWriteTime += threadWriteArray[i]._perfResults._time;
+    }
+    printf("Queue: <==STATS_DUMP==>\n");
+    printf("Queue: EntriesCurrent=%zu\n", gpQueue->_stats_EntriesCurrent);
+    printf("Queue: EntriesMax=%zu\n", gpQueue->_stats_EntriesMax);
+    printf("Queue: MemUsageCurrent=%zu\n", gpQueue->_stats_MemUsageCurrent);
+    printf("Queue: MemUsageMax=%zu\n", gpQueue->_stats_MemUsageMax);
+    printf("Queue: MemSizeCurrent=%zu\n", gpQueue->_stats_MemSizeCurrent);
+    printf("Queue: MemSizeMin=%zu\n", gpQueue->_stats_MemSizeMin);
+    printf("Queue: MemSizeMax=%zu\n", gpQueue->_stats_MemSizeMax);
+    printf("Queue:DESTROY\n");
+    queue_destroy(gpQueue);
+    for(i = 0;i < numElements;i++) {
+        if(gReadCfg._array[i] != 1)
+            printf("readArray[%zu] read %zu times insted once\n", i, gReadCfg._array[i]);
+    }
+    //timerTimespecDiff(&readTimeStart, &ReadTimeEnd, &ReadTime);
+    //timerTimespecDiff(&writeTimeStart, &writeTimeEnd, &WriteTime);
+    printf("WRITE[TIME in s]: %f\n", (float)cumulativeWriteTime/1000000000.0f/(float)gWriteCfg._numThreads);
+    printf("WRITE[THROUPGHPUT in item/s]: %zu\n", (size_t)((float)numElements/((float)cumulativeWriteTime/1000000000.0f)));
+    printf("READ[TIME in s]: %f\n", (float)cumulativeReadTime/1000000000.0f/(float)gReadCfg._numThreads);
+    printf("READ[THROUPGHPUT in item/s]: %zu\n", (size_t)((float)numElements/((float)cumulativeReadTime/1000000000.0f)));
+    return 0;
 }
