4a424ec1800e040847cb307679ba5bb6dd01aa7c
grzegorz.kasprzyszak <gregorkmail@gmail.com>
2019-07-05 22:27:37 +0200

    Test PDBs scripts

--- oracle/bin/db_PDB_clone.sh
+++ oracle/bin/db_PDB_clone.sh
@@ -10,7 +10,7 @@ CONNECTION_STRING="sys/$SYS_PASSWORD@$TNS_NAME as sysdba"
 sqlplus -s $CONNECTION_STRING <<EOF
 whenever sqlerror exit sql.sqlcode;
 
-create pluggable database $TO_DB_NAME from $FROM_DB_NAME
+create pluggable database $TO_DB_NAME from $FROM_DB_NAME;
 alter pluggable database $TO_DB_NAME open read write;
 alter pluggable database $TO_DB_NAME save state;
 exit;
--- oracle/bin/db_PDB_create.sh
+++ oracle/bin/db_PDB_create.sh
@@ -2,17 +2,20 @@
 
 TNS_NAME=$1
 SYS_PASSWORD=$2
-FROM_DB_NAME=$3
-TO_DB_NAME=$4
+DB_NAME=$3
+ADM_LOGINNAME=$4
+ADM_PASSWORD=$5
 
 CONNECTION_STRING="sys/$SYS_PASSWORD@$TNS_NAME as sysdba"
 
 sqlplus -s $CONNECTION_STRING <<EOF
 whenever sqlerror exit sql.sqlcode;
 
-create pluggable database $TO_DB_NAME from $FROM_DB_NAME
-alter pluggable database $TO_DB_NAME open read write;
-alter pluggable database $TO_DB_NAME save state;
+create pluggable database $DB_NAME admin user $ADM_LOGINNAME identified by $ADM_PASSWORD;
+alter pluggable database $DB_NAME open read write;
+alter pluggable database $DB_NAME save state;
+alter session set container = $DB_NAME;
+grant DBA to $ADM_LOGINNAME container = current;
 exit;
 EOF
 
