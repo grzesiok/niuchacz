f9cc639b62d2ee3891d29e72e3eb5f378ec8e2e8
grzegorz.kasprzyszak <gregorkmail@gmail.com>
2019-07-05 22:47:30 +0200

    Add configuration listener for CDB to auto register PDBs

--- oracle/bin/db_CDB_init.sh
+++ oracle/bin/db_CDB_init.sh
@@ -9,6 +9,8 @@ alter system set db_files=2000 scope=spfile;
 alter system set db_create_file_dest='/u01data' scope=spfile;
 shutdown immediate;
 startup;
+alter system set local_listener='(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=localhost)(PORT=1521)))';
+alter system register;
 exit;
 EOF
 
