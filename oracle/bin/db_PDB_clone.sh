6e512ad125f0fd6540c9281ead5d8b5bdc2df753
grzegorz.kasprzyszak <gregorkmail@gmail.com>
2019-08-01 14:35:08 +0200

    Copy all data in PATH_PREFIX SRC to DST PDB

--- oracle/bin/db_PDB_clone.sh
+++ oracle/bin/db_PDB_clone.sh
@@ -8,7 +8,17 @@ TO_DB_NAME=$4
 
 CONNECTION_STRING="sys/$SYS_PASSWORD@$TNS_NAME as sysdba"
 
-PATH_PREFIX=$(sqlplus -s $CONNECTION_STRING <<EOF
+FROM_PATH_PREFIX=$(sqlplus -s $CONNECTION_STRING <<EOF
+whenever sqlerror exit sql.sqlcode;
+set head off
+
+select value||'/'||'$TNS_NAME'||'/'||'$FROM_DB_NAME'||'/' into :path_prefix from v\$parameter where name = 'db_create_file_dest';
+exit;
+EOF
+)
+FROM_PATH_PREFIX=$(echo $FROM_PATH_PREFIX|tr -d '\n')
+echo "FROM_PATH_PREFIX=$FROM_PATH_PREFIX"
+TO_PATH_PREFIX=$(sqlplus -s $CONNECTION_STRING <<EOF
 whenever sqlerror exit sql.sqlcode;
 set head off
 
@@ -16,11 +26,12 @@ select value||'/'||'$TNS_NAME'||'/'||'$TO_DB_NAME'||'/' into :path_prefix from v
 exit;
 EOF
 )
-PATH_PREFIX=$(echo $PATH_PREFIX|tr -d '\n')
-echo "PATH_PREFIX=$PATH_PREFIX"
-mkdir -p $PATH_PREFIX
+TO_PATH_PREFIX=$(echo $TO_PATH_PREFIX|tr -d '\n')
+echo "TO_PATH_PREFIX=$TO_PATH_PREFIX"
+mkdir -p $TO_PATH_PREFIX
+cp -R $FROM_PATH_PREFIX/* $TO_PATH_PREFIX
 sqlplus -s $CONNECTION_STRING <<EOF
-create pluggable database $TO_DB_NAME from $FROM_DB_NAME path_prefix='$PATH_PREFIX';
+create pluggable database $TO_DB_NAME from $FROM_DB_NAME path_prefix='$TO_PATH_PREFIX';
 alter pluggable database $TO_DB_NAME open read write;
 alter pluggable database $TO_DB_NAME save state;
 exit;
